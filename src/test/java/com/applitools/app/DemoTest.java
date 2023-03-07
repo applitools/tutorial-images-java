20b1043a7bc3a7bf6e2a4f592f2adbc71faac0fd
Plakhota <90543395+Plakhota@users.noreply.github.com>
2023-03-07 14:42:31 +0200

    Update DemoTest.java
    Add beforeAll, afterAll, Runner, Configuration.

--- src/test/java/com/applitools/app/DemoTest.java
+++ src/test/java/com/applitools/app/DemoTest.java
@@ -1,9 +1,15 @@
 package com.applitools.app;
 
+import com.applitools.eyes.EyesRunner;
+import com.applitools.eyes.TestResultsSummary;
+import com.applitools.eyes.config.Configuration;
 import com.applitools.eyes.images.Eyes;
 import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.images.ImageRunner;
 import com.applitools.eyes.images.Target;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.net.URL;
 import java.awt.image.BufferedImage;
@@ -12,32 +18,48 @@ import javax.imageio.ImageIO;
 import java.nio.file.Paths;
 
 public class DemoTest {
+    private static EyesRunner runner = new ImageRunner();
+    private static Configuration config = new Configuration();
+
+    @BeforeAll
+    public static void setUp(){
+        //config.setServerUrl("https://eyes.applitools.com/"); //set by default
+        // Define the OS and hosting application to identify the baseline.
+        config.setHostOS("Windows 11");
+        config.setHostApp("My app");
+
+    }
+
     @Test
     public void test() {
         
-        Eyes eyes = new Eyes();
-
-        // // Define the OS and hosting application to identify the baseline.
-        eyes.setHostOS("Windows 10");
-        eyes.setHostApp("My maxthon browser");
+        Eyes eyes = new Eyes(runner);
+        eyes.setConfiguration(config);
 
         try {
             // Start the test with a viewport size of 800x600.
             eyes.open("Demo App - Images Java", "Smoke Test - Images Java", new RectangleSize(800, 600));
 
-            // Load page image and validate.
+            // Load image.
             BufferedImage img = ImageIO.read(new URL("https://i.ibb.co/bJgzfb3/applitools.png"));
 
-            // // Visual validation.
+            // Visual validation.
             eyes.check("Image buffer", Target.image(img));
 
             // End visual UI testing.
-            eyes.close();
+            eyes.closeAsync();
         } catch(IOException ex){
             System.out.println(ex);
         } finally {
             // If the test was aborted before eyes.close was called, ends the test as aborted.
             eyes.abortIfNotClosed();
         }
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        //wait for Applitools test to finish and fetch the results
+        TestResultsSummary  results = runner.getAllTestResults();
+        System.out.println(results);
     }
 }
